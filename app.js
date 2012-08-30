f4e423bfa2ad2506236c19155e57f00c9abfbee1
Pratik Moona <pratikmoona@gmail.com>
2012-08-30 16:36:19 +0530

    Added skeleton of processor

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var argv = require('optimist')
 	.usage('Usage: -p [path]')
 	.demand(['p'])
 	.argv;
-var util = require('./lib/util/environ');
-var loader = require('./lib/plugin_loader/loader');
-
+require('./lib/util/environ');
+var loader = require('./lib/plugin_loader/core');
+var processor = require('./lib/processor/core');
+logger.debug("Reading file from " + argv.p);
 var commands = loader.load(argv.p);
-
+processor.process(commands);
 logger.debug(JSON.stringify(commands));
