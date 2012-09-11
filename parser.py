725fd7ae12a1b602e6229039d6d88bea7b2bcbd6
Anirudh Kumar <anirudhhkumarr@gmail.com>
2012-09-11 20:04:29 +0530

    modified parser to parse courses

--- parser.py
+++ parser.py
@@ -3,8 +3,8 @@ import json
 import fileinput
 
 class parseError(Exception):
-	def __init__(self):
-		self.command=None
+	def __init__(self, obj):
+		obj.state = 0
 
 class parser:
 	def __init__(self):
@@ -15,48 +15,53 @@ class parser:
 		self.params = {}
 
 	def parse(self, command):
-		if command == "START module" and self.state == 0:
-			self.name = "add"
-			self.state = 1
-		elif command == "START params" and self.state == 1:
-			self.params = {}
-			self.params['type'] = 'module'
-			self.state = 2
-		elif command == "START attributes" and self.state == 2:
-			self.params['attributes'] = []
-			self.state = 3
-		elif command == "END module" and self.state == 1:
-			self.parsedObject['name']=self.name
-			self.parsedObject['params']=self.params
-			self.parsedProgram.append(self.parsedObject)
-			self.parsedObject = {}
-			self.state = 0
-		elif command == "END params" and self.state == 2:
-			self.state = 1
-		elif command == "END attributes" and self.state == 3:
-			self.state = 2	
-		elif self.state == 2:
-			command = command.split(" ",2)
-			if command[0] == "DEFINE":
-				temp = {}
-				self.params[command[1]]=command[2].strip('"')
+		command = command.split(" ",2)
+		if command[0] == "START":
+			if command[1] == "course" and self.state == 0:
+				self.name = "add"
+				self.params = {}
+				self.params['type'] = 'course'
+				self.state = 1
+			elif command[1] == "module" and self.state == 0:
+				self.name = "add"
+				self.params = {}
+				self.params['type'] = 'module'
+				self.state = 1
+			elif command[1] == "params" and self.state == 1:
+				self.state = 2
+			elif command[1] == "attributes" and self.state == 2:
+				self.params['attributes'] = []
+				self.state = 3
 			else:
-				raise parseError()
+				raise parseError(self)
+		elif command[0] == "END":
+			if command[1] == "course" and self.state == 1:
+				self.parsedObject['name']=self.name
+				self.parsedObject['params']=self.params
+				self.parsedProgram.append(self.parsedObject)
+				self.parsedObject = {}
 				self.state = 0
-			self.state = 2
-		elif self.state == 3:
-			command = command.split(" ",2)
-			if command[0] == "ADD":
-				temp = {}
-				temp[command[1]]=command[2].strip('"')
-				self.params['attributes'].append(temp)
+			elif command[1] == "module" and self.state == 1:
+				self.parsedObject['name']=self.name
+				self.parsedObject['params']=self.params
+				self.parsedProgram.append(self.parsedObject)
+				self.parsedObject = {}
+				self.state = 0
+			elif command[1] == "params" and self.state == 2:
+				self.state = 1
+			elif command[1] == "attributes" and self.state == 3:
+				self.state = 2
 			else:
-				raise parseError()
-				self.state = 0
-			self.state = 3
+				raise parseError(self)
+		elif command[0] == "DEFINE" and self.state == 2:
+			temp = {}
+			self.params[command[1]]=command[2].strip('"')
+		elif command[0] == "ADD" and self.state == 3:
+			temp = {}
+			temp[command[1]]=command[2].strip('"')
+			self.params['attributes'].append(temp)
 		else:
-			raise parseError()
-			self.state = 0
+			raise parseError(self)
 
 def main():
 	p = parser()
